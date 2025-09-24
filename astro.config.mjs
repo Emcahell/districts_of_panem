64bf49f0cf35a84cd0e1f13139effa780212c1c9
emcahell <emmanuelltowers@gmail.com>
2025-09-24 19:25:57 -0400

    install sitemap astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,16 @@ import tailwindcss from '@tailwindcss/vite';
 
 import preact from '@astrojs/preact';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://districts-of-panem.vercel.app/',
+
   vite: {
     plugins: [tailwindcss()]
   },
 
-  integrations: [preact()]
+  integrations: [preact(), sitemap()]
+
 });
